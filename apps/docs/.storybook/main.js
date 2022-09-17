802a9b99f6c1a0b72f5f73dd31fe3cdeeb59f025
Yony Calsin <helloyonycalsin@gmail.com>
2022-09-17 13:00:38 -0500

    fix: build storybook correctly

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -1,33 +1,31 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
-  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  framework: "@storybook/react",
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-vite",
+    builder: '@storybook/builder-vite',
   },
   async viteFinal(config, { configType }) {
-
     // https://github.com/eirslett/storybook-builder-vite/issues/55
-    config.root = path.dirname(require.resolve("@storybook/builder-vite"));
+    config.root = path.dirname(require.resolve('@storybook/builder-vite'))
 
-    config.server.fsServe = undefined;
-    
+    if (config?.server?.fsServe) {
+      config.server.fsServe = undefined
+    }
+
     // customize the Vite config here
     return {
       ...config,
       resolve: {
         alias: [
           {
-            find: "@chakra-ui-kbar/core",
-            replacement: path.resolve(
-              __dirname,
-              "../../../packages/chakra-ui-kbar-core/"
-            ),
+            find: '@chakra-ui-kbar/core',
+            replacement: path.resolve(__dirname, '../../../packages/chakra-ui-kbar-core/'),
           },
         ],
       },
-    };
+    }
   },
-};
+}
